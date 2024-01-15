6c6c7db8f989ce502dfbdbf35eb6cd46165f972b
Draven <valdevisor@gmail.com>
2024-01-15 23:05:07 +0800

    fix home file

--- src/home.js
+++ src/home.js
@@ -5,8 +5,14 @@ import createFooter from "./footer";
 export default function createHome() {
   const contentDiv = document.getElementById("content");
 
+  const heroImg = document.createElement("img");
+  heroImg.src = "/dist/img/background-home-desktop.jpg";
+  heroImg.alt = "hero image background";
+
   const home = document.createElement("div");
   home.classList.add("hero-container");
+
+  home.style.cursor = "url(/dist/img/rocket.png), auto";
 
   const pageContent = document.createElement("div");
   pageContent.classList.add("page-content");
@@ -17,26 +23,27 @@ export default function createHome() {
   const paragraph = document.createElement("h3");
   paragraph.textContent = "The taste of home, in every dish";
 
-  const heroBtn = document.createElement("button");
-  heroBtn.classList.add("btn");
-
-  heroBtn.addEventListener("mouseover", (e) => {
-    const x = e.pageX - heroBtn.offsetLeft;
-    const y = e.pageY - heroBtn.offsetTop;
-
-    heroBtn.style.setProperty("--xPos", x + "px");
-    heroBtn.style.setProperty("--yPos", y + "px");
-  });
-
   const heroLink = document.createElement("a");
-  heroLink.textContent = "order now";
   heroLink.classList.add("heroBtn");
+
+  const linkSpan = document.createElement("span");
+  linkSpan.textContent = "order now";
+
+  // heroLink.addEventListener("mouseover", (e) => {
+  //   const x = e.pageX - heroLink.offsetLeft;
+  //   const y = e.pageY - heroLink.offsetTop;
+
+  //   heroLink.style.setProperty("--xPos", x + "px");
+  //   heroLink.style.setProperty("--yPos", y + "px");
+  // });
 
   contentDiv.appendChild(pageContent);
   pageContent.appendChild(home);
+  home.appendChild(heroImg);
   home.appendChild(homeContent);
   homeContent.appendChild(paragraph);
-  homeContent.appendChild(heroBtn);
-  heroBtn.appendChild(heroLink);
+  homeContent.appendChild(heroLink);
+  // heroBtn.appendChild(heroLink);
+  heroLink.appendChild(linkSpan);
   pageContent.appendChild(createFooter());
 }
